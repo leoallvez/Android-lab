04a5b83fef532722bcbfd1f2165ca784e3377b50
Leonardo Alves <leoallvez@hotmail.com>
2019-04-27 23:41:17 -0300

    commit de pequenas alterações

--- lista-de-compras/app/src/main/java/br/com/listadecompras/MainActivity.kt
+++ lista-de-compras/app/src/main/java/br/com/listadecompras/MainActivity.kt
@@ -2,6 +2,8 @@ package br.com.listadecompras
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,19 +15,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Implementado o adaptador;
-        val produtoAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
         //definindo o adaptador na lista;
-        list_view_produtos.adapter = produtoAdapter
+        list_view_produtos.adapter = produtosAdapter
 
         //definindo domouvinte do botão;
         btn_inserir.setOnClickListener {
             val produto = txt_produto.text.toString();
             if(produto.isNotEmpty()) {
-                produtoAdapter.add(produto)
+                produtosAdapter.add(produto)
                 txt_produto.text.clear();
             } else {
                 txt_produto.error = "Preencha um valor";
             }
+        }
+
+        list_view_produtos.setOnItemLongClickListener { adapterView: AdapterView<*>, view: View, position: Int, id: Long ->
+
+            val item = produtosAdapter.getItem(position)
+            produtosAdapter.remove(item)
+            true
         }
     }
 }
