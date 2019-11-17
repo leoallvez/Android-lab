93414f5d6a5331ba0923ebdd9dd7855fcb84bae7
leoallvez <leoallvez@hotmail.com>
2019-11-16 23:56:12 -0300

    estudo avançado

---
+++ layouts/app/src/main/java/dominando/android/layouts/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package dominando.android.layouts.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dominando.android.layouts.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ layouts/app/src/main/java/dominando/android/layouts/model/Cliente.kt
@@ -0,0 +1,5 @@
+package dominando.android.layouts.model
+
+import java.io.Serializable
+
+class Cliente(var codigo: Int, var nome: String?): Serializable
